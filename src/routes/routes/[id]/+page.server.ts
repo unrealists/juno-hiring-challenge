abba11e06ce980c45d30cf1d3501e3a75ccc2dfb
Jan Franciszek Cieslak <jfcieslak@gmail.com>
2023-07-27 19:11:04 +0200

    dynamic url

--- src/routes/routes/[id]/+page.server.ts
+++ src/routes/routes/[id]/+page.server.ts
@@ -1,8 +1,8 @@
 import type { CurrentPosition } from '$lib/getCurrentPosition';
 import { error } from '@sveltejs/kit';
 
-export const load = async ({ params }) => {
-	const res = await fetch(`http://localhost:5173/api/routes/${params.id}`);
+export const load = async ({ params, url }) => {
+	const res = await fetch(`${url.origin}/api/routes/${params.id}`);
 	if (res.status !== 200) throw error(res.status, { message: res.statusText });
 	const data: CurrentPosition = await res.json();
 	return data;
